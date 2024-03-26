31990610f6e4d93a3c67ff41e01b9f0bab5ef0b3
AlexBilka <aleksandrbelka@gmail.com>
2024-03-26 11:04:01 +0200

    first settings

---
+++ src/js/1-timer.js
@@ -0,0 +1,44 @@
+// підключення бібліотеки
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// =======================================================
+
+// const data = [
+//   {
+
+//   },
+// ];
+
+// =======================================================
+
+// const gallery = document.querySelector('.gallery');
+
+// // додаємо розмітку галереї в HTML
+// gallery.insertAdjacentHTML('beforeend', createMarkup(images));
+
+// // створення розмітки галереї
+// function createMarkup(arr) {
+//   return arr
+//     .map(
+//       ({ preview, original, description }) =>
+//         `<li class="gallery-item">
+//   <a class="gallery-link" href='${original}'>
+//     <img
+//       class="gallery-image"
+//       src='${preview}'
+//       alt='${description}'
+//     />
+//   </a>
+// </li>`
+//     )
+//     .join('');
+// }
+
+// =======================================================
+
+// Ініціалізація бібліотеки
+// const lightbox = new SimpleLightbox('.gallery-link', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+// });
