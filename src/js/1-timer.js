9211b68275b530f72d5618866c26bb6472f203a7
AlexBilka <aleksandrbelka@gmail.com>
2024-03-26 18:15:07 +0200

    first timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,44 +1,81 @@
-// підключення бібліотеки
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+// підключення бібліотек
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 // =======================================================
 
-// const data = [
-//   {
-
-//   },
-// ];
+let userSelectedDate;
 
 // =======================================================
 
-// const gallery = document.querySelector('.gallery');
+const startBtn = document.querySelector('button');
+const showTime = document.querySelectorAll('.value');
+const inputTime = document.querySelector('#datetime-picker');
 
-// // додаємо розмітку галереї в HTML
-// gallery.insertAdjacentHTML('beforeend', createMarkup(images));
-
-// // створення розмітки галереї
-// function createMarkup(arr) {
-//   return arr
-//     .map(
-//       ({ preview, original, description }) =>
-//         `<li class="gallery-item">
-//   <a class="gallery-link" href='${original}'>
-//     <img
-//       class="gallery-image"
-//       src='${preview}'
-//       alt='${description}'
-//     />
-//   </a>
-// </li>`
-//     )
-//     .join('');
-// }
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    const timeValid = userSelectedDate - new Date();
+    if (timeValid < 1) {
+      iziToast.error({
+        position: 'topCenter',
+        message: `Please choose a date in the future`,
+      });
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+};
 
 // =======================================================
 
-// Ініціалізація бібліотеки
-// const lightbox = new SimpleLightbox('.gallery-link', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+startBtn.disabled = true;
+
+flatpickr('#datetime-picker', options);
+
+startBtn.addEventListener('click', event => {
+  const intervalId = setInterval(() => {
+    let timeInterval = userSelectedDate - new Date();
+
+    if (timeInterval < 1) {
+      clearInterval(intervalId);
+      return;
+    }
+    startBtn.disabled = true;
+    inputTime.disabled = true;
+
+    const timer = convertMs(timeInterval);
+
+    showTime[0].textContent = timer.days.toString().padStart(2, '0');
+    showTime[1].textContent = timer.hours.toString().padStart(2, '0');
+    showTime[2].textContent = timer.minutes.toString().padStart(2, '0');
+    showTime[3].textContent = timer.seconds.toString().padStart(2, '0');
+  }, 1000);
+});
