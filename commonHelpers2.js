18c8e3d34d14b0629abe59534c822b96d3d5c8d0
AlexBilka <114955573+AlexBilka@users.noreply.github.com>
2024-03-27 12:38:37 +0000

    Deploying to gh-pages from @ AlexBilka/goit-js-hw-10@a2c4cf68c65125648ae5cd01be18361c9e182d9f 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i as s}from"./assets/vendor-77e16229.js";const m=document.querySelector(".form");m.addEventListener("submit",l);function l(e){e.preventDefault();const i=e.target.state.value,t=e.target.delay.value;new Promise((o,r)=>{setTimeout(()=>{i==="fulfilled"?o(s.success({title:"Resolve",message:`✅ Fulfilled promise in ${t}ms`,position:"topRight"})):r(s.error({title:"Reject",message:`❌ Rejected promise in ${t}ms`,position:"topLeft"}))},t)})}
+//# sourceMappingURL=commonHelpers2.js.map
